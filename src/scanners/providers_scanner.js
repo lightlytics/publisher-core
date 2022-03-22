d1683c77e5e22cfbc785fe3aa0fa5dca29519ac5
romi <romi@lightlytics.com>
2022-03-22 18:32:11 +0200

    add comments

--- src/scanners/providers_scanner.js
+++ src/scanners/providers_scanner.js
@@ -6,31 +6,31 @@ export function providersScanner(addData) {
 
   function processLine(line) {
     const sanitizedLine = String(line).trim();
+    // ignore comments
     if (sanitizedLine.startsWith("#")) return;
 
+    // find module block and count it. increase the block counter each { occurrence
     if (sanitizedLine.startsWith("module ")) {
       moduleName = sanitizedLine.match(/"([^"]+)"/)[1];
       blockCnt = 1;
+    } else if (blockCnt > 0 && sanitizedLine.includes("{")) {
+      blockCnt += (sanitizedLine.match(/{/g) || []).length;
     }
 
-    else if (blockCnt > 0 && sanitizedLine.includes("{")){
-      blockCnt+=(sanitizedLine.match(/{/g) || []).length;
-    }
-
+    // find the providers block and add the block lines to the providersBlockLines until the end of block
     if (blockCnt > 0 && sanitizedLine.startsWith("providers = {")) {
       providers = true;
       providersBlockLines = "";
-    }
-
-    else if (providers && ! sanitizedLine.includes("}")) {
+    } else if (providers && !sanitizedLine.includes("}")) {
       providersBlockLines += line;
     }
 
+    // decrease the block counter each } occurrence. if it is the end of the module block and there is provider data, add it by the module name
     if (blockCnt > 0 && sanitizedLine.includes("}")) {
-      blockCnt-=(sanitizedLine.match(/}/g) || []).length;
-      providers = false
-      if (blockCnt === 0 &&  providersBlockLines) {
-        addData({[moduleName]: providersBlockLines})
+      blockCnt -= (sanitizedLine.match(/}/g) || []).length;
+      providers = false;
+      if (blockCnt === 0 && providersBlockLines) {
+        addData({ [moduleName]: providersBlockLines });
       }
     }
   }
